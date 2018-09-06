1dcc08b39e5f00d62ef0a216f7c1439b9848977f
Anshul Sharma <anshul.an@gmail.com>
2018-09-06 17:46:30 -0400

    Add BlueCat license to REST workflows
    Update README formatting.

--- Community/rest_api/__init__.py
+++ Community/rest_api/__init__.py
@@ -1,5 +1,24 @@
-# Copyright 2018 BlueCat Networks. All rights reserved.
-# -*- coding: utf-8 -*-
+# Copyright 2018 BlueCat Networks (USA) Inc. and its affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# By: Xiao Dong (xdong@bluecatnetworks.com)
+#     Anshul Sharma (asharma@bluecatnetworks.com)
+# Date: 06-09-2018
+# Gateway Version: 18.9.1
+# Description: This workflow will provide access to a REST based API for Gateway.
+#              Once imported, documentation for the various end points available can
+#              be viewed by navigating to /api/v1/.
 
 type = 'api'
 sub_pages = [
