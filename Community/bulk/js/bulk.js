bc246ece25d93da5eaf50df2c0a75e0eecfbdd35
Chris Storz <chrisjstorz@gmail.com>
2019-03-12 15:24:08 -0700

    Update bulk.js

--- Community/bulk/js/bulk.js
+++ Community/bulk/js/bulk.js
@@ -1,4 +1,4 @@
-// Copyright 2018 BlueCat Networks. All rights reserved.
+// Copyright 2019 BlueCat Networks. All rights reserved.
 // JavaScript for your page goes in here.
 $( document ).ready(function() {
     $('#file').on("change", function(){
