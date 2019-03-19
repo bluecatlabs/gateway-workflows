081b56260e24ed9da4301bf12117050979b24859
Chris Storz <cstorz@bluecatnetworks.com>
2019-03-19 15:23:00 -0400

    Added workflow and copyrights

---
+++ Community/itsm_api/__init__.py
@@ -0,0 +1,12 @@
+# Copyright 2018 BlueCat Networks. All rights reserved.
+# -*- coding: utf-8 -*-
+
+type = 'api'
+sub_pages = [
+    {
+        'name'        : 'itsm_api_page',
+        'title'       : u'itsm_api',
+        'endpoint'    : 'itsm_api/itsm_api_endpoint',
+        'description' : u'itsm_api'
+    },
+]
