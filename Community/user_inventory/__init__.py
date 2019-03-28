13a17f7882bb08595a75b2289d703f4a8234db7a
Murtaza Haider <mhaider@bluecatnetworks.com>
2019-03-28 17:19:06 -0400

    REST workflow that generates a list of BAM users and their permissions. Output report is in CSV format and is emailed to Mail Admins. Workflow is meant to be used in conjunction with Rundeck as a report scheduling example.

---
+++ Community/user_inventory/__init__.py
@@ -0,0 +1,12 @@
+# Copyright 2018 BlueCat Networks. All rights reserved.
+# -*- coding: utf-8 -*-
+
+type = 'api'
+sub_pages = [
+    {
+        'name'        : 'user_inventory_page',
+        'title'       : u'user_inventory',
+        'endpoint'    : 'user_inventory/user_inventory_endpoint',
+        'description' : u'user_inventory'
+    },
+]
