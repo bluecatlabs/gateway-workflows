a5cd2b29066ca102fd0a0f9958cd7891d709777e
Murtaza Haider <mhaider@bluecatnetworks.com>
2019-03-28 10:36:05 -0400

    Create A record and deploy to DNS with REST

---
+++ Community/create_a_record/__init__.py
@@ -0,0 +1,12 @@
+# Copyright 2018 BlueCat Networks. All rights reserved.
+# -*- coding: utf-8 -*-
+
+type = 'api'
+sub_pages = [
+    {
+        'name'        : 'create_a_record_page',
+        'title'       : u'create_a_record',
+        'endpoint'    : 'create_a_record/create_a_record_endpoint',
+        'description' : u'create_a_record'
+    },
+]
