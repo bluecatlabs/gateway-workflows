212733480356b1a1cb5ec58b3976312ecc0cf11b
Chris Meyer <cmeyer@bluecatnetworks.com>
2019-04-01 09:26:26 -0400

    Add manage_dhcp workflow to Community folder

---
+++ Community/manage_dhcp/__init__.py
@@ -0,0 +1,12 @@
+# Copyright 2017 BlueCat Networks. All rights reserved.
+# -*- coding: utf-8 -*-
+
+type = 'api'
+sub_pages = [
+    {
+        'name'        : 'manage_dhcp',
+        'title'       : 'manage_dhcp',
+        'endpoint'    : 'manage_dhcp/endpoint',
+        'description' : 'manage_dhcp'
+    },
+]
