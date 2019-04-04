595d9f4154cb1e09e541941fe4a941733f3133be
haiders5 <murtazahaider@gmail.com>
2019-04-03 22:06:50 -0400

    Update acl_inventory_wtform_fields.py
    Fixing date

--- Community/acl_inventory/acl_inventory_wtform_fields.py
+++ Community/acl_inventory/acl_inventory_wtform_fields.py
@@ -1,4 +1,4 @@
-# Copyright 2017 BlueCat Networks. All rights reserved.
+# Copyright 2019 BlueCat Networks. All rights reserved.
 from bluecat.wtform_fields import CustomSelectField
 from .acl_inventory_server_endpoints import get_acls
 
