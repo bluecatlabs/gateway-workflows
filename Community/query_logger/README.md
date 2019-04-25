dc5cfb9257dcefaa981c2a6f7e5b6bc1d4f98952
ryutamura <rtamura@bluecatnetworks.com>
2019-04-25 12:51:36 +0900

    Updated and added workflows

---
+++ Community/query_logger/README.md
@@ -0,0 +1,80 @@
+# Query Logger
+This workflow will send DNS query logs from the BlueCat DNS Edge CI (Customer Instance) to a designated Syslog server in a syslog format.   
+Only the logs that match DNS Edge policies will be sent.    
+
+## Prerequisites
+1. **BAM Default Configuration**  
+This workflow will be using the default configuration value in `/portal/bluecat_portal/config.py` in BlueCat Gateway container.  To set the default configuration, in BlueCat Gateway, go to Administration > Configurations > General Configuration.  
+In General Configuration, select the BAM Settings tab and enter the configuration name under "Default Configuration:" and save.  
+![screenshot](img/BAM_default_settings.jpg?raw=true "BAM_default_settings")  
+
+2. **Additional Python3 Library**  
+This workflow requires the python3 *"arrow"*, *"redis"* and the *"apscheduler"* library.  
+Install the libraries using PIP3 inside the BlueCat Gateway container.
+```
+$pip3 install arrow redis apscheduler
+
+```  
+
+3. **Additional Python Code**  
+This workflow requires addtional python code.  
+Copy the directory *"dnsedge"* under `additional/dnsedge` to `/portal/bluecat_portal/customizations/integrations/` inside the BlueCat Gateway container.  
+
+
+## Usage   
+
+1. **Set Parameters**  
+Set the following parameters:  
+    - DNS Edge URL:  
+      This URL will be the BlueCat DNS Edge CI.  
+      The URL should be *"https://api-<Your_Edge_CI_URL>"*
+
+    - API Token:  
+      This will the API token to login to BlueCat DNS Edge CI.  
+
+    - Syslog Server IP Address:  
+      This will be the IP address of the server to send the DNS query logs in a syslog format.  
+      Make sure that the specified IP address is reachable from BlueCat Gateway.  
+
+    - Poll Interval (seconds):  
+      Enter desired polling intervals.  
+      The minimum interval is five seconds.  
+
+![screenshot](img/query_logger1.jpg?raw=true "query_logger1")  
+
+Click *SUBMIT*  
+Check that the *Configuration was successfully saved* message appears.  
+
+![screenshot](img/query_logger2.jpg?raw=true "query_logger2")  
+
+Check to see whether logs are being sent to the designated server.  
+
+---
+
+## Additional   
+
+1. **Language**  
+You can switch to a Japanese menu by doing the following.  
+    1. Create *ja.txt* in the BlueCat Gateway container.  
+    ```
+    cd /portal/Administration/create_workflow/text/  
+    cp en.txt ja.txt  
+    ```  
+    2. In the BlueCat Gateway Web UI, go to Administration > Configurations > General Configuration.   
+    In General Configuration, select the *Customization* tab.  
+    Under *Language:* type in `ja` and save.  
+    ![screenshot](img/langauge_ja.jpg?raw=true "langauge_ja")  
+
+2. **Appearance**  
+This will make the base html menus a little bit wider.  
+    1. Copy all files under the directory `additional/templates` to `/portal/templates` inside the Bluecat Gateway container.
+
+
+
+## Credits  
+By: Akira Goto (agoto@bluecatnetworks.com)  
+Date: 2019-04-25  
+Gateway Version: 18.10.2
+
+## License
+©2019 BlueCat Networks (USA) Inc. and its affiliates (collectively ‘ BlueCat’). All rights reserved. This document contains BlueCat confidential and proprietary information and is intended only for the person(s) to whom it is transmitted. Any reproduction of this document, in whole or in part, without the prior written consent of BlueCat is prohibited.
---
+++ Community/query_logger/query_logger_page.py
@@ -0,0 +1,129 @@
+# Copyright 2019 BlueCat Networks (USA) Inc. and its affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Various Flask framework items.
+import os
+import sys
+import codecs
+
+from flask import url_for, redirect, render_template, flash, g
+from wtforms.validators import DataRequired, IPAddress, URL, NumberRange
+from wtforms import SubmitField
+
+from bluecat.wtform_extensions import GatewayForm
+from bluecat.wtform_fields import CustomStringField
+from bluecat import route, util
+import config.default_config as config
+from main_app import app
+
+from .query_logger import QueryLogger
+
+def module_path():
+    encoding = sys.getfilesystemencoding()
+    return os.path.dirname(os.path.abspath(__file__))
+
+class GenericFormTemplate(GatewayForm):
+    """
+    Generic form Template
+
+    Note:
+        When updating the form, remember to make the corresponding changes to the workflow pages
+    """
+    workflow_name = 'query_logger'
+    workflow_permission = 'query_logger_page'
+
+    text=util.get_text(module_path(), config.language)
+    invalid_url_message=text['invalid_url_message']
+    require_message=text['require_message']
+
+    edge_url = CustomStringField(
+        label=text['label_edge_url'],
+        is_disabled_on_start=False,
+        validators=[DataRequired(message=require_message), URL(message=invalid_url_message)],
+        render_kw={"placeholder": "https://api-<Edge Instance>.bluec.at"}
+    )
+
+    edge_token = CustomStringField(
+        label=text['label_edge_token'],
+        is_disabled_on_start=False,
+        validators=[DataRequired(message=require_message)]
+    )
+
+    syslog_server = CustomStringField(
+        label=text['label_syslog_server'],
+        is_disabled_on_start=False,
+        validators=[DataRequired(message=require_message), IPAddress()]
+    )
+
+    poll_interval = CustomStringField(
+        label=text['label_poll_interval'],
+        is_disabled_on_start=False,
+        validators=[DataRequired(message=require_message)]
+    )
+
+    submit = SubmitField(label=text['label_submit'])
+
+
+# The workflow name must be the first part of any endpoints defined in this file.
+# If you break this rule, you will trip up on other people's endpoint names and
+# chaos will ensue.
+@route(app, '/query_logger/query_logger_endpoint')
+@util.workflow_permission_required('query_logger_page')
+@util.exception_catcher
+def query_logger_query_logger_page():
+    form = GenericFormTemplate()
+
+    query_logger = QueryLogger.get_instance(debug=True)
+    form.edge_url.data = query_logger.get_value('edge', 'url')
+    form.edge_token.data = query_logger.get_value('edge', 'token')
+    form.syslog_server.data = query_logger.get_value('datalog', 'server')
+    form.poll_interval.data = query_logger.get_value('poll', 'interval')
+
+    return render_template(
+        'query_logger_page.html',
+        form=form,
+        text=util.get_text(module_path(), config.language),
+        options=g.user.get_options(),
+    )
+
+@route(app, '/query_logger/form', methods=['POST'])
+@util.workflow_permission_required('query_logger_page')
+@util.exception_catcher
+def query_logger_query_logger_page_form():
+    form = GenericFormTemplate()
+    if form.validate_on_submit():
+        text=util.get_text(module_path(), config.language)
+        query_logger = QueryLogger.get_instance(debug=True)
+
+        query_logger.set_value('edge', 'url', form.edge_url.data)
+        query_logger.set_value('edge', 'token', form.edge_token.data)
+        query_logger.set_value('datalog', 'server', form.syslog_server.data)
+        query_logger.set_value('poll', 'interval', int(form.poll_interval.data))
+
+        query_logger.save()
+        query_logger.register_job()
+
+        # Put form processing code here
+        g.user.logger.info('SAVED')
+        flash(text['saved_message'], 'succeed')
+
+        return redirect(url_for('query_loggerquery_logger_query_logger_page'))
+    else:
+        g.user.logger.info('Form data was not valid.')
+        return render_template(
+            'query_logger_page.html',
+            form=form,
+            text=util.get_text(module_path(), config.language),
+            options=g.user.get_options(),
+        )
