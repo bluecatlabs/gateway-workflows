6babe8d3d2f1e182a81e48fe05c5e1cfac2ab1fb
Bill Morton <34247411+billfmorton@users.noreply.github.com>
2019-11-13 15:25:53 -0500

    Update cmdb_config.py

--- Community/ServiceNow CMDB/cmdb_configuration/cmdb_config.py
+++ Community/ServiceNow CMDB/cmdb_configuration/cmdb_config.py
@@ -1,4 +1,4 @@
-servicenow_url = 'https://ven02854.service-now.com'
+servicenow_url = 'https://service-now.com'
 servicenow_username = 'bill.morton'
 servicenow_secret_file = 'workflows/ServiceNow CMDB/cmdb_configuration/.secret'
 servicenow_max_query_results = '1000'
