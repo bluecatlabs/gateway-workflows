7a083578a85f28629d88580b846871034ecf5c46
bmorton <bmorton@bluecatnetworks.com>
2020-02-07 09:24:50 -0500

    Adding missing files

---
+++ Community/bulk_record_export/js/bulk_record_export_page.js
@@ -0,0 +1,5 @@
+// Copyright 2019 BlueCat Networks. All rights reserved.
+// JavaScript for your page goes in here.
+function custom_js_table_loaded(data) {
+    console.log("on_complete custom function called");
+}
