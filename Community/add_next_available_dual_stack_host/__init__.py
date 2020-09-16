8f02718139d835e49c822fa34ad8eefe7c2873d9
Erik Pineiro <epineiro@bluecatnetworks.com>
2020-09-16 12:29:09 -0400

    Adding Add Next Available Dual Stack Host workflow

---
+++ Community/add_next_available_dual_stack_host/__init__.py
@@ -0,0 +1,12 @@
+# Copyright 2020 BlueCat Networks. All rights reserved.
+# -*- coding: utf-8 -*-
+
+type = 'ui'
+sub_pages = [
+    {
+        'name'        : 'add_next_available_dual_stack_host_page',
+        'title'       : u'Add Next Available Dual Stack Host',
+        'endpoint'    : 'add_next_available_dual_stack_host/add_next_available_dual_stack_host_endpoint',
+        'description' : u'Add a host record with the next available IPv4 and IPv6 addresses from the chosen networks'
+    },
+]
---
+++ Community/add_next_available_dual_stack_host/js/add_next_available_dual_stack_host_page.js
@@ -0,0 +1,8 @@
+// Copyright 2020 BlueCat Networks. All rights reserved.
+$(document).ready(function() {
+
+});
+
+function clearForm() {
+    document.getElementById("add_next_available_dual_stack_host_page_form").reset();
+}
